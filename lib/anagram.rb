c98710613f994ab5a823baf012b09366cc5930ce
Dani Lyn <dlyn1110@gmail.com>
2018-05-01 00:25:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,6 +4,11 @@ attr_accessor :word
 
 def initialize(word)
   @word = word
+end
 
-
+def match(array)
+  array.select do |letters|
+    letters.split("").sort == @word.split("").sort
+  end
+ end
 end
